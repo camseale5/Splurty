fb3b4c8cc58a9ea8a36218ebe13ab3ee94596574
Mark Seale <camseale5@yahoo.com>
2019-10-11 06:55:04 +0000

    Save the data on submit

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -11,4 +11,14 @@ def new
 
   end
 
+  def create
+    Quote.create(quote_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def quote_params
+    params.require(:quote).permit(:saying, :author)
+  end
 end
